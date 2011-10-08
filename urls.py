d7801eedfd675c9c770d3b5125a818dacd2ec322
Jose Ernesto Davila Pantoja <josernestodavila@ubuntu.com>
2011-10-08 17:40:07 -0600

    URL para 'smart_selects'

--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     #(r'^$', direct_to_template, {'template': 'index.html'}),
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^chaining/', include('smart_selects.urls')),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('registration.backends.default.urls')),
