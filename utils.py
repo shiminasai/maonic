b4d9e8d3fa2c8c45c0a814387904f369ec5453e9
Jose Ernesto Davila Pantoja <josernestodavila@ubuntu.com>
2011-10-26 05:15:35 -0600

    Utilerias para exportar a xls

---
+++ utils.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+from django.http import HttpResponse
+
+def save_as_xls(request):
+    tabla = request.POST['tabla']    
+    response = render_to_response('xls.html', {'tabla': tabla, })
+    response['Content-Disposition'] = 'attachment; filename=tabla.xls'
+    response['Content-Type'] = 'application/vnd.ms-excel'
+    response['Charset'] ='UTF-8'
+    return response
