9998f79a2cff566f9cd851ba7731c6823344771e
sacrac <crocha09.09@gmail.com>
2013-04-26 17:19:33 -0600

    fix

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
 from django.contrib import admin
+from sorl.thumbnail.admin import AdminImageMixin
 from models import *
 
+class NoticiasAdmin(admin.ModelAdmin, AdminImageMixin):
+	search_fields = ['titulo']
+	date_hierarchy = 'fecha'
 
-admin.site.register(Noticias)
+admin.site.register(Noticias, NoticiasAdmin)
