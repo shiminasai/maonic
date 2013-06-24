d77225449d759db2b6f03cf08c536a1387b1ec18
sacrac <crocha09.09@gmail.com>
2013-06-24 17:40:58 -0600

    fix lo de los videos

--- videos/views.py
+++ videos/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from .models import Video
+from noticias.models import Noticias
 
 def lista_videos(request):
-	objectos = Video.objects.all()
+	objetos = Video.objects.all()
+	ultimas_noticias = Noticias.objects.order_by('-id')[:4]
 
-	return render_to_response('videos/videos_list.html', {'objects':objectos},
+	return render_to_response('videos/videos_list.html', {'objetos':objetos,
+									'ultimas_noticias':ultimas_noticias},
 							 context_instance=RequestContext(request))
