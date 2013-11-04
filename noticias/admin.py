1d1f4f1b269510526de6141f5f1c4e2b69af4197
sacrac <crocha09.09@gmail.com>
2013-11-04 16:10:10 -0600

    agregando los pages

--- noticias/admin.py
+++ noticias/admin.py
@@ -18,6 +18,26 @@ class FotoInlineAdmin(admin.StackedInline):
 class PortadaAdmin(ImageCroppingMixin, admin.ModelAdmin):
 	inlines = [FotoInlineAdmin]
 
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+
+from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
+from django.contrib.flatpages.admin import FlatpageForm as FlatpageFormOld
+ 
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+
+class FlatpageForm(FlatpageFormOld):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = FlatPage
+ 
+class FlatPageAdmin(FlatPageAdminOld):
+    form = FlatpageForm
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
+
 admin.site.register(Portada,PortadaAdmin)
 admin.site.register(FotosPortadas, fotoAdmin)
 admin.site.register(Noticias, NoticiasAdmin)
